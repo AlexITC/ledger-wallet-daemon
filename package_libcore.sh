5eebfd0fc12e20d86badedfabfe0d0e9628da512
Yacine Badiss <yacine.badiss@ledger.fr>
2018-12-17 17:20:39 +0100

    wip: updated dylib to new version of lib core fixing the no funds error

--- package_libcore.sh
+++ package_libcore.sh
@@ -6,9 +6,9 @@ pkg_dir=`mktemp -d`
 current_dir=`pwd`
 build_dir=$current_dir/../lib-ledger-core-build
 libcore_dir=$current_dir/../lib-ledger-core
-scala_api_dir=$current_dir/ledger-core-binding/src/main/scala/co/ledger/core
-java_api_dir=$current_dir/ledger-core-binding/src/main/java/co/ledger/core
-dest_dir=$current_dir/ledger-core-binding/lib
+scala_api_dir=$current_dir/src/main/scala/co/ledger/core
+java_api_dir=$current_dir/src/main/java/co/ledger/core
+dest_dir=$current_dir/lib
 
 echo "Updating libraries"
 # Create structure of directory used to build libledger-core.jar
@@ -20,15 +20,17 @@ cp $build_dir/core/src/libledger-core.dylib $pkg_dir/resources/djinni_native_lib
 
 # Build the actual jar
 cd $pkg_dir
-jar cvf libledger-core.jar resources/
-cp libledger-core.jar $dest_dir/libledger-core.jar
-cd -
+jar cvf ledger-lib-core.jar resources/
+cp $pkg_dir/ledger-lib-core.jar $dest_dir/ledger-lib-core.jar
+cd $current_dir
 rm -rf $pkg_dir
 echo "Libraries updated"
 
 echo "Updating APIs"
-rm $java_api_dir/*
-rm $scala_api_dir/implicits/*
+mkdir -p $java_api_dir
+mkdir -p $scala_api_dir/implicits
+rm -f $java_api_dir/*
+rm -f $scala_api_dir/implicits/*
 cp $libcore_dir/api/core/scala/* $scala_api_dir/implicits/
 cp $libcore_dir/api/core/java/* $java_api_dir
 echo "API updated"
